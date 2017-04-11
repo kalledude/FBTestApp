a370f617b7a0a43078813c1c1b5b1cdd27992df9
Karl Härnqvist <kalle_rec@hotmail.com>
2017-04-11 23:16:27 +0200

    Initial commit
    Added google services, model class etc.

---
+++ FirebaseTestApp/app/src/main/java/Models/User.java
@@ -0,0 +1,22 @@
+package Models;
+
+/**
+ * Created by Kalle on 2017-04-11.
+ */
+
+public class User {
+
+    public String name;
+    public String email;
+    public User(){
+
+    }
+
+    public User(String name, String email)
+    {
+        this.name = name;
+        this.email = email;
+    }
+
+
+}
---
+++ FirebaseTestApp/app/src/main/java/com/example/kalle/firebasetestapp/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.kalle.firebasetestapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    private String userId;
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
